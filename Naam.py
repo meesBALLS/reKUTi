0ffc52ec9fada11c8187f1c1262eb77f8b8eebf8
MeesBJV2 <90202167+MeesBJV2@users.noreply.github.com>
2023-10-04 15:04:05 +0200

    GRID

--- Naam.py
+++ Naam.py
@@ -3,26 +3,55 @@ from PIL.ImageDraw import Draw
 from PIL.ImageTk   import PhotoImage
 from PIL           import Image
 
+scherm_size = 1000
+plaatje_size = 600
+speelveld_grootte = 6
+
 scherm = Frame()
-scherm.master.title("$projectname$")
+scherm.master.title("reKUTi")
 scherm.configure(background="lightblue")
-scherm.configure(width=1000, height=1000)
+scherm.configure(width=scherm_size, height=scherm_size)
 scherm.pack()
 
-# met een Image kun je een plaatje opslaan in het geheugen
-plaatje = Image.new(mode="RGBA", size=(600,600))
+plaatje = Image.new( mode="RGBA" , size=(plaatje_size,plaatje_size)) 
+draw = Draw(plaatje)
 
-# maar om complexere figuren te tekenen heb je een Draw nodig
-tekenaar = Draw(plaatje)
-tekenaar.ellipse( ((40,40),(130,90)), "blue")
+afbeelding = Label(scherm) 
+afbeelding.place(x=0, y=0) 
+afbeelding.configure(width=plaatje_size, height=plaatje_size)
 
-# het PIL-Image moet eerst nog worden omgezet naar een Tk-PhotoImage
-omgezetPlaatje = PhotoImage(plaatje)
+
+
+def Speelveld_tekenen(x):
+    global plaatje_size
+    lijn_pos = plaatje_size / x
+    print(lijn_pos)
+    for i in range(1,x):
+        draw.line(((lijn_pos*i,0),(lijn_pos*i,plaatje_size)),fill="blue")
+        draw.line(((0,lijn_pos*i),(plaatje_size,lijn_pos*i)),fill="blue")
+    global foto
+    foto = PhotoImage(plaatje)
+    afbeelding.configure(image=foto)
+
+
+def teken_stukken(x,y,speler):
+    if speler == 1:
+      draw.ellipse(((x,y),(x+600/speelveld_grootte,y+600/speelveld_grootte)),fill="red")
+    elif speler == 2:
+      draw.ellipse(((x,y),(x+600/speelveld_grootte,y+600/speelveld_grootte)),fill="blue")
+    global foto
+    foto = PhotoImage(plaatje)
+    afbeelding.configure(image=foto)
+    
+
+Speelveld_tekenen(speelveld_grootte)
+
+
+def muisKlik(ea):
+    teken_stukken(ea.x,ea.y,1)
+
 
 # een Label kan ook gebruikt worden om een PhotoImage te laten zien
-afbeelding = Label(scherm)
-afbeelding.place(x=10, y=10)
-afbeelding.configure(background="white")
-afbeelding.configure(image=omgezetPlaatje)
 
+afbeelding.bind("<Button-1>", muisKlik)
 scherm.mainloop()
